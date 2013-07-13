2b2497ccc689882a8d1091b3617860079c41950e
KaSt <k@ka.st>
2013-07-13 11:36:17 +0200

    leds: hardcode max_current Instead of relying to scripts to set the max_current at boot time, hardcode the values in the drivers like it was done for zeus board

--- arch/arm/mach-msm/leds-mango.c
+++ arch/arm/mach-msm/leds-mango.c
@@ -16,39 +16,39 @@ static struct as3676_platform_led as3676_leds_mapping[] = {
 		.name = "lcd-backlight",
 		.sinks = BIT(AS3676_SINK_01),
 		.flags = AS3676_FLAG_ALS | AS3676_FLAG_WAIT_RESUME,
-		.max_current = 25000,
+		.max_current = 21000,
 		.default_brightness = LED_FULL,
 	},
 	{
 		.name = "button-backlight",
 		.sinks = BIT(AS3676_SINK_RGB2) | BIT(AS3676_SINK_RGB3),
 		.flags = AS3676_FLAG_ALS_GROUP2,
-		.max_current = 10000,
+		.max_current = 500,
 	},
 	{
 		.name = "keyboard-backlight",
 		.sinks = BIT(AS3676_SINK_30) | BIT(AS3676_SINK_31) |
 				BIT(AS3676_SINK_32) | BIT(AS3676_SINK_33),
 		.flags = AS3676_FLAG_ALS_GROUP2,
-		.max_current = 25000,
+		.max_current = 5000,
 	},
 	{
 		.name = "red",
 		.sinks = BIT(AS3676_SINK_41),
 		.flags = AS3676_FLAG_RGB | AS3676_FLAG_BLINK,
-		.max_current = 25000,
+		.max_current = 5000,
 	},
 	{
 		.name = "green",
 		.sinks = BIT(AS3676_SINK_42),
 		.flags = AS3676_FLAG_RGB | AS3676_FLAG_BLINK,
-		.max_current = 25000,
+		.max_current = 5000,
 	},
 	{
 		.name = "blue",
 		.sinks = BIT(AS3676_SINK_43),
 		.flags = AS3676_FLAG_RGB | AS3676_FLAG_BLINK,
-		.max_current = 25000,
+		.max_current = 5000,
 	},
 };
 
