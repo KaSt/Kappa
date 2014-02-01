672901e6153c7179936af93a78923d0769b078cb
KaSt <k@ka.st>
2014-02-01 15:09:54 +0100

    Revert "ext4: add missing save_error_info() to ext4_error()"
    This reverts commit f9d61d215b04005d46e8c3c15a6640ce0b601d96.

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -345,7 +345,6 @@ void ext4_error(struct super_block *sb, const char *function,
 	vprintk(fmt, args);
 	printk("\n");
 	va_end(args);
-	save_error_info(sb, function, line);
 
 	ext4_handle_error(sb);
 }
