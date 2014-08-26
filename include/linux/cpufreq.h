a9000e9fb186e20092a2c566f9225cb1e4f69633
Ka <k@ka.st>
2014-08-26 12:28:30 +0200

    Added SmartMax Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -406,6 +406,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand2;
 extern struct cpufreq_governor cpufreq_gov_smartass_h3;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartass_h3)
 
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
 
 #endif                                                                                                                                        
 
